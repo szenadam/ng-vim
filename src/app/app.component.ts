a861aef909e573e5d14dcebee185b112ea3834af
szenadam <szenadam1@gmail.com>
2019-12-08 13:30:00 +0100

    Listen to keydowns for changing editor mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 import { SetMode } from './actions';
 
@@ -8,14 +8,24 @@ import { SetMode } from './actions';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   public mode$: Observable<string>;
+  private keypress: Subscription;
+  @ViewChild('editorArea', {static: false}) editorArea: ElementRef;
 
   constructor(private store: Store<{ count: number }>) {
     this.mode$ = this.store.pipe(select('mode'));
   }
 
-  public setMode(mode: string) {
-    this.store.dispatch(new SetMode(mode));
+  ngAfterViewInit(): void {
+    this.keypress = fromEvent(this.editorArea.nativeElement, 'keydown').subscribe((event: KeyboardEvent) => {
+      if (event.key === 'i') {
+        this.store.dispatch(new SetMode('insert'));
+      } else if (event.key === 'n') {
+        this.store.dispatch(new SetMode('normal'));
+      } else if (event.key === 'v') {
+        this.store.dispatch(new SetMode('visual'));
+      }
+    });
   }
 }
